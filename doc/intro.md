5f1181ce6bc6eb9649192dafd40c5ff74115bf25
amasclans <albert.masclans@globalzitro.com>
2018-03-07 11:49:41 +0100

    lein app

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to porc
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/porc/core_test.clj
@@ -0,0 +1,7 @@
+(ns porc.core-test
+  (:require [clojure.test :refer :all]
+            [porc.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
